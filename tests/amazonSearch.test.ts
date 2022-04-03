0b79bfddf9b51861dd480a59c8024546677b2866
Daniel Lukas <dtlukasart@gmail.com>
2022-04-03 13:13:43 -0400

    added test to varify search box was found and filled

--- tests/amazonSearch.test.ts
+++ tests/amazonSearch.test.ts
@@ -3,7 +3,7 @@ import { expect } from "chai";
 import { Browser, BrowserContext, chromium, Locator, Page } from "playwright";
 
 
-describe("amazonQuery tests", () => {
+describe("amazonSearch tests", () => {
     let browser: Browser, context: BrowserContext, page: Page;
 
     beforeEach(async function() {
@@ -25,4 +25,20 @@ describe("amazonQuery tests", () => {
         console.log(title)
         expect(title).to.equal("Amazon.com. Spend less. Smile more.");
     })
+
+    it("should find search input and add query", async () => {
+        await page.goto("https://www.amazon.com/");
+        await page.locator('[aria-label="Search"]').click();
+        await page.locator('[aria-label="Search"]').fill("nvidia 3060");
+       
+        const actulText: String = await page.inputValue('input#twotabsearchtextbox');
+        console.log("actualText", actulText)
+        
+        expect(actulText).to.equal("nvidia 3060");
+    });
+
+    // await Promise.all([
+    //     page.waitForNavigation(/*{ url: 'https://www.amazon.com/s?k=nvidia+3060&crid=21FUVI91FZNLT&sprefix=nvidia+3060%2Caps%2C83&ref=nb_sb_noss_1' }*/),
+    //     page.locator('input:has-text("Go")').click()
+    //   ]);
 });
