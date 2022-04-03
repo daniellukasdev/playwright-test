4edfb3eb1742878df634f48667afd0ebf6cbe9f6
Daniel Lukas <dtlukasart@gmail.com>
2022-04-03 19:06:05 -0400

    dynamically searches for queries from list

--- src/amazonSearch.ts
+++ src/amazonSearch.ts
@@ -1,16 +1,31 @@
 import { Browser, BrowserContext, chromium, Page } from "playwright";
 
+const queries: string[] = ["nvidia 3060", "nvidia 3070", "nvidia 3080"];
 
 (async () => {
-    const browser: Browser = await chromium.launch({
-                    headless: false,
-                });
-    const context: BrowserContext = await browser.newContext();
-    const page: Page = await context.newPage();
-    await page.goto("https://www.amazon.com/");
-    const title = await page.title();
-    
-    console.log(title);
-    await browser.close();
-})();
+    for (const query of queries) {
+            // creates new browser and context
+            const browser: Browser = await chromium.launch({
+                headless: false,
+            });
+            const context: BrowserContext = await browser.newContext();
 
+            // creates new page for amazon.com
+            const page: Page = await context.newPage();
+            await page.goto("https://www.amazon.com/");
+            console.log("query", query)
+            await page.waitForSelector('[aria-label="Search"]');
+            await page.locator('[aria-label="Search"]').click();
+            await page.locator('[aria-label="Search"]').fill(`${query}`);
+
+            await Promise.all([
+                page.waitForNavigation(),
+                page.locator('input:has-text("Go")').click()
+            ]);
+
+            await page.waitForSelector('[aria-label="Search"]');
+            const searchedText: string = await page.inputValue('[aria-label="Search"]');
+            console.log(searchedText);
+            await browser.close();
+    }
+})();
